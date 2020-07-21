56405785fc74f50d98cca691fb56886816e0e780
Turner Kirk <voxpipes@gmail.com>
2020-07-21 15:39:42 -0700

    updated Leaflet version, re-structured files, added public API key for Mapbox

--- src/config.js
+++
@@ -1,2 +0,0 @@
-// API key
-const API_KEY = "insert_mapbox_api_key_here";
---
+++ static/config/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoibWFwc3R5bGUxMCIsImEiOiJjanduc3FicG8wbndxNDRyMmlqdzcwMnFyIn0.jG0mABWxTQMHCj7aqW7qew";
